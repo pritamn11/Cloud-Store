fad58a3e51ba86d3d0af2e4d2887c1b78ab66729
pritamn11 <pritam.narwade11@gmail.com>
2024-12-23 11:51:31 +0530

    django project added

---
+++ cloudstore/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cloudstore project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloudstore.settings')
+
+application = get_asgi_application()
